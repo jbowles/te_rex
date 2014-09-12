39a89bce7bd6bbd4dd95f4fea159c456099f0195
joshua bowles <bowlesling@gmail.com>
2014-09-12 10:46:29 -0600

    remove internal deps that are not needed

--- lib/trex.rb
+++ lib/trex.rb
@@ -1,8 +1,6 @@
-require_relative "trex/stop_word"
 require_relative "trex/corpus"
 require_relative "trex/corpus_formats/brown_corpus_file"
 require_relative "trex/n_gram"
-require_relative "trex/simple_grammar"
 require_relative "trex/bayes_data"
 require_relative "trex/bayes"
 
