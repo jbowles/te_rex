b01de5cf167ba03cd168452ea44dc1cb39803c75
joshua bowles <bowlesling@gmail.com>
2014-09-12 10:29:49 -0600

    include version file

--- lib/trex.rb
+++ lib/trex.rb
@@ -1,10 +1,11 @@
 require_relative "trex/stop_word"
 require_relative "trex/corpus"
 require_relative "trex/corpus_formats/brown_corpus_file"
-require_relative"trex/n_gram"
+require_relative "trex/n_gram"
 require_relative "trex/simple_grammar"
-require_relative"trex/bayes_data"
-require_relative"trex/bayes"
+require_relative "trex/bayes_data"
+require_relative "trex/bayes"
+require_relative "trex/version"
 
 module Trex
 end
