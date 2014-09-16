e3cafb74da130ea9abb447d333d2e86485a7c5dc
joshua bowles <bowlesling@gmail.com>
2014-09-16 11:09:02 -0600

    move ngrams into topic branch

--- lib/trex.rb
+++ lib/trex.rb
@@ -1,7 +1,6 @@
 require_relative "trex/format"
 require_relative "trex/stop_word"
 require_relative "trex/alpha_num"
-require_relative "trex/n_gram"
 require_relative "trex/bayes_data"
 require_relative "trex/bayes"
 
