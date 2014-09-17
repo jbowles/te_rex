e2f9643f2573dd088f06ecc1f6d329fd6f518de0
joshua bowles <bowlesling@gmail.com>
2014-09-16 19:06:08 -0600

    update branch

---
+++ lib/train/nonrefund_train.rb
@@ -0,0 +1,29 @@
+module Trex
+  module Train
+    NONREFUND_TRAIN =[
+      "This reservation is non-refundable. Cancellations or changes made at any time are subject to a 100% charge.",
+      "This reservation is non-refundable. Cancellations or changes made at any time are subject to a 100% charge.",
+      "This reservation is non-refundable. Cancellations or changes made at any time are subject to a 100% charge.",
+      "This reservation is non-refundable. Cancellations or changes made at any time are subject to a 100% charge.",
+      "This reservation is non-refundable. Cancellations or changes made at any time are subject to a 100% charge.",
+      "This reservation is non-refundable. Cancellations or changes made at any time are subject to a 100% charge.",
+      "This rate is non-refundable and cannot be changed or cancelled - if you do choose to change or cancel this booking you will not be refunded any of the payment.",
+      "This rate is non-refundable and cannot be changed or cancelled - if you do choose to change or cancel this booking you will not be refunded any of the payment.",
+      "This rate is non-refundable and cannot be changed or cancelled - if you do choose to change or cancel this booking you will not be refunded any of the payment.",
+      "This rate is non-refundable and cannot be changed or cancelled - if you do choose to change or cancel this booking you will not be refunded any of the payment.",
+      "This rate is non-refundable and cannot be changed or cancelled - if you do choose to change or cancel this booking you will not be refunded any of the payment.",
+      "This rate is non-refundable and cannot be changed or cancelled - if you do choose to change or cancel this booking you will not be refunded any of the payment.",
+      "For the room type and rate that you've selected you are not allowed to change or cancel your reservation. If you cancel your room you will still be charged for the full reservation amount.",
+      "For the room type and rate that you've selected you are not allowed to change or cancel your reservation. If you cancel your room you will still be charged for the full reservation amount.",
+      "For the room type and rate that you've selected you are not allowed to change or cancel your reservation. If you cancel your room you will still be charged for the full reservation amount.",
+      "For the room type and rate that you've selected you are not allowed to change or cancel your reservation. If you cancel your room you will still be charged for the full reservation amount.",
+      "For the room type and rate that you've selected you are not allowed to change or cancel your reservation. If you cancel your room you will still be charged for the full reservation amount.",
+      "For the room type and rate that you've selected you are not allowed to change or cancel your reservation. If you cancel your room you will still be charged for the full reservation amount.",
+      "This reservation is non-refundable. Cancellations or changes made at any time are subject to a 100% charge. We are sorry but refunds are not available for early check-out.",
+      "This reservation is non-refundable. Cancellations or changes made at any time are subject to a 100% charge. We are sorry but refunds are not available for early check-out.",
+      "This reservation is non-refundable. Cancellations or changes made at any time are subject to a 100% charge. We are sorry but refunds are not available for early check-out.",
+      "This reservation is non-refundable. Cancellations or changes made at any time are subject to a 100% charge. We are sorry but refunds are not available for early check-out.",
+      "This reservation is non-refundable. Cancellations or changes made at any time are subject to a 100% charge. We are sorry but refunds are not available for early check-out.",
+    ]
+  end
+end
