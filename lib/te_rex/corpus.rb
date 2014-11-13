a5fc521e6538ef402156ece8976f0faf5d5f6ee0
joshua bowles <bowlesling@gmail.com>
2014-11-13 16:09:50 -0700

    more tests for corpus and sentence partiioning

--- lib/te_rex/corpus.rb
+++ lib/te_rex/corpus.rb
@@ -2,7 +2,7 @@ module TeRex
   module Corpus
     class Body
 
-      attr_reader :set, :sample_size, :training, :testing, :format_klass, :category_klass
+      attr_reader :set, :sample_size, :training, :testing, :format_klass, :category_klass, :total_sentences
 
       def initialize(glob: "", partition: :file, format_klass: NilClass, category_klass: NilClass)
         @glob = glob
@@ -16,17 +16,27 @@ module TeRex
         define_set
         case @partition
         when /file/
-          @sample_size = (@set.count.to_f * 0.75).round
-          @training = partition_training_by_file
-          @testing = partition_test_by_file
+          file_partition
         else
-          #super_set = build_superset
-          corpus_set = partition_files_for_sentence
-          #@sample_size = (superset.count.to_f * 0.75).round
-          @sample_size = 0.0
-          @training = partition_training_by_sentence(corpus_set)
-          @testing = partition_test_by_sentence(corpus_set)
+          sentence_partition
         end
+      end
+
+      def file_partition
+        @sample_size = (@set.count.to_f * 0.75).round
+        @training = partition_training_by_file
+        @testing = partition_test_by_file
+        count_all
+      end
+
+      def sentence_partition
+        #super_set = build_superset
+        corpus_set = partition_files_for_sentences
+        #@sample_size = (superset.count.to_f * 0.75).round
+        @sample_size = 0.0
+        @training = partition_training_by_sentence(corpus_set)
+        @testing = partition_test_by_sentence(corpus_set)
+        count_all
       end
 
       def define_set
@@ -63,9 +73,15 @@ module TeRex
       end
 
       def build_superset
-        @set.reduce() do |memo,formatter|
+        @set.reduce([]) do |memo,formatter|
           memo << formatter.sentences
         end.flatten
+      end
+
+      def count_all
+        counter = 0
+        @set.map{|f| counter += f.sentences.count}
+        @total_sentences = counter
       end
 
     end
--- test/corpus_test.rb
+++ test/corpus_test.rb
@@ -3,13 +3,16 @@ class CorpusTest < MicroTest::Test
   class MockErrorClassifier
   end
 
-    #@pos_corpus = TeRex::Corpus::Body.new("corpora/movie_reviews/pos/cv**", TeRex::Format::BasicFile)
   @@error_corpus = TeRex::Corpus::Body.new(glob: "test/test_modules/*.csv", format_klass: TeRex::Format::ErrorFile, category_klass: MockErrorClassifier)
   @@error_corpus.build
 
   test "Corpus has correct data before building" do
     assert @@error_corpus.format_klass.name == "TeRex::Format::ErrorFile"
     assert @@error_corpus.category_klass.name == "CorpusTest::MockErrorClassifier"
+  end
+
+  test "total count of sentences is correct" do
+    assert @@error_corpus.total_sentences == 12
   end
 
   test "ratio of training to testing is within 70%" do
@@ -19,8 +22,36 @@ class CorpusTest < MicroTest::Test
 
   test "sentence counts are correct" do
     assert @@error_corpus.set.count == 3
-    @@error_corpus.training.count == 10
-    @@error_corpus.testing.count == 7
+    assert @@error_corpus.training.count == 12
+    assert @@error_corpus.testing.count == 8
+  end
+
+  @@sent_corpus = TeRex::Corpus::Body.new(glob: "test/test_modules/*.csv", partition: :sentence, format_klass: TeRex::Format::ErrorFile, category_klass: MockErrorClassifier)
+  @@sent_corpus.build
+
+  test "Corpus has correct data before building" do
+    assert @@sent_corpus.format_klass.name == "TeRex::Format::ErrorFile"
+    assert @@sent_corpus.category_klass.name == "CorpusTest::MockErrorClassifier"
+  end
+
+  test "total count of sentences is correct" do
+    assert @@sent_corpus.total_sentences == 12
+  end
+
+  test "ratio of training to total is about 75%" do
+    ratio = @@sent_corpus.training.count.to_f / @@sent_corpus.total_sentences
+    assert (72...77).map{|i| i}.include?((ratio * 100).to_i)
+  end
+
+  test "ratio of training to total is about 25%" do
+    ratio = @@sent_corpus.testing.count.to_f / @@sent_corpus.total_sentences
+    assert (22...27).map{|i| i}.include?((ratio * 100).to_i)
+  end
+
+  test "sentence counts are correct" do
+    assert @@sent_corpus.set.count == 3
+    assert @@sent_corpus.training.count == 9
+    assert @@sent_corpus.testing.count == 3
   end
 
 end
