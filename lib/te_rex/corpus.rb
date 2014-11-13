3be35ef42a0c958387ff3fb13cc0792a08c7ec36
joshua bowles <bowlesling@gmail.com>
2014-11-13 15:41:48 -0700

    test and train data sets by sentences

--- lib/te_rex/corpus.rb
+++ lib/te_rex/corpus.rb
@@ -4,35 +4,67 @@ module TeRex
 
       attr_reader :set, :sample_size, :training, :testing, :format_klass, :category_klass
 
-      def initialize(glob: "", format_klass: NilClass, category_klass: NilClass)
+      def initialize(glob: "", partition: :file, format_klass: NilClass, category_klass: NilClass)
         @glob = glob
         @format_klass = format_klass
         @category_klass = category_klass
+        @partition = partition
       end
 
+      #@sample_size = (@set.count * 0.75).round
       def build
         define_set
-        @training = partition_train
-        @testing = partition_test
+        case @partition
+        when /file/
+          @sample_size = (@set.count.to_f * 0.75).round
+          @training = partition_training_by_file
+          @testing = partition_test_by_file
+        else
+          #super_set = build_superset
+          corpus_set = partition_files_for_sentence
+          #@sample_size = (superset.count.to_f * 0.75).round
+          @sample_size = 0.0
+          @training = partition_training_by_sentence(corpus_set)
+          @testing = partition_test_by_sentence(corpus_set)
+        end
       end
 
       def define_set
         @set ||= Dir[@glob].map do |file|
           @format_klass.new(file)
         end
-        @sample_size = (@set.count * 0.75).round
         @set
       end
 
-      def partition_train
+      def partition_training_by_file
         @set[0..@sample_size].map do |file|
           file.scanner
         end.flatten
       end
 
-      def partition_test
+      def partition_test_by_file
         @set[(@sample_size - 1)..-1].map do |file|
           file.scanner
+        end.flatten
+      end
+
+      def partition_files_for_sentences
+        @set.map do |file|
+          file.scanner
+        end.flatten
+      end
+
+      def partition_training_by_sentence(c_set)
+        c_set.sample(c_set.count * 0.75)
+      end
+
+      def partition_test_by_sentence(c_set)
+        c_set.sample(c_set.count * 0.25)
+      end
+
+      def build_superset
+        @set.reduce() do |memo,formatter|
+          memo << formatter.sentences
         end.flatten
       end
 
--- test/corpus_test.rb
+++ test/corpus_test.rb
@@ -3,17 +3,24 @@ class CorpusTest < MicroTest::Test
   class MockErrorClassifier
   end
 
-  @@error_corpus= TeRex::Corpus::Body.new(glob: '../test_modules/*.csv', format_klass: TeRex::Format::ErrorFile, category_klass: MockErrorClassifier)
+    #@pos_corpus = TeRex::Corpus::Body.new("corpora/movie_reviews/pos/cv**", TeRex::Format::BasicFile)
+  @@error_corpus = TeRex::Corpus::Body.new(glob: "test/test_modules/*.csv", format_klass: TeRex::Format::ErrorFile, category_klass: MockErrorClassifier)
   @@error_corpus.build
 
   test "Corpus has correct data before building" do
-    corpus= TeRex::Corpus::Body.new(glob: 'test_modules/*.csv', format_klass: TeRex::Format::ErrorFile, category_klass: MockErrorClassifier)
-    assert corpus.format_klass.name == "TeRex::Format::ErrorFile"
-    assert corpus.category_klass.name == "CorpusTest::MockErrorClassifier"
+    assert @@error_corpus.format_klass.name == "TeRex::Format::ErrorFile"
+    assert @@error_corpus.category_klass.name == "CorpusTest::MockErrorClassifier"
   end
 
-  test "data from csv files looks good" do
-    @@error_corpus.set == false
+  test "ratio of training to testing is within 70%" do
+    ratio = @@error_corpus.testing.count.to_f / @@error_corpus.training.count.to_f
+    assert (60...80).map{|i| i}.include?((ratio * 100).to_i)
+  end
+
+  test "sentence counts are correct" do
+    assert @@error_corpus.set.count == 3
+    @@error_corpus.training.count == 10
+    @@error_corpus.testing.count == 7
   end
 
 end
